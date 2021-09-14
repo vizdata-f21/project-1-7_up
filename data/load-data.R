595a36e9f811373242ec51ff1b145fee703ebaf4
Margaret Reed <margaret.reed24@gmail.com>
2021-09-14 18:31:57 +0000

    cleaned up questions

--- data/load-data.R
+++ data/load-data.R
@@ -4,6 +4,8 @@ library(readr)
 install.packages("tidytuesdayR")
 tuesdata <- tidytuesdayR::tt_load('2021-02-09')
 
-for(x in c("income_mean", "lifetime_earn", "student_debt", "retirement", "home_owner", "race_wealth", "income_time", "income_limits", "income_aggregate", "income_distribution")){
-  write.csv(tuesdata$get(x), file = paste0("data/", x, ".csv"))
+for(x in c("income_mean", "lifetime_earn", "student_debt", "retirement",
+           "home_owner", "race_wealth", "income_time", "income_limits",
+           "income_aggregate", "income_distribution")){
+  write.csv(tuesdata[[x]], file = paste0("data/", x, ".csv"), row.names=F)
 }
